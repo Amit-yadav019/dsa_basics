029e47dc804e6b5e58c12bf7b0fcb105b0ba041b
Amit Kumar <2024ugpi019@nitjsr.ac.in>
2026-06-25 22:28:37 +0530

    learning calculus

---
+++ 01_input_output.py
@@ -0,0 +1,16 @@
+# print("Day 1 of learning DSA")
+# print("Day 1 of learning DSA")
+
+
+# def main():
+#     print('Day 1 of learning DSA')
+
+# if __name__=="__main__":
+#     main()
+
+
+x,y = map(int,input("Enter  X and Y :").split())
+
+print(f"X is :{x} and Y is {y} ")
+
+
