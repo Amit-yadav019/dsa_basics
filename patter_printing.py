c74e5cc56fc86908a833077c148f5d190eec601c
Amit Kumar <2024ugpi019@nitjsr.ac.in>
2026-07-03 23:04:19 +0530

    pattern printing day1

---
+++ patter_printing.py
@@ -0,0 +1,76 @@
+#          * * * *
+#          * * * *
+#          * * * *
+#          * * * *
+
+# for i in range(0,4,1):
+#     for j in range(0,4,1):
+#         print(" * ",end=' ' )
+
+#     print()
+
+
+
+# for taking printing element from users 
+
+# n = int(input("Enter any integers whose square pattern for n index  "))
+# p = input(" Enter that you want to print ")
+# for i in range(0,n):
+#     for j in range(0,n):
+#         print(f" {p} ",end=' ')
+
+#     print()    
+
+
+#.        *
+#.        * *
+#.        * * *
+#.        * * * *
+#.        * * * * *
+
+# for i in range(0,5):
+#     for j in range(0,i+1):
+#         print(" * ", end='')
+
+#     print()    
+
+#  * 
+#  *  * 
+#  *  *  * 
+#  *  *  *  * 
+#  *  *  *  *  * 
+
+
+#.   1 
+#.   1 2
+#.   1 2 3
+#.   1 2 3 4 
+#.   1 2 3 4 5
+# n = int(input("enter whats n "))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(f" {j} ",end='')
+#     print()    
+
+
+n = int(input("enter whats n "))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(f" {i} ",end=' ')
+    print()   
+
+
+
+
+
+
+
+
+
+
+
+
+   
+     
+
+      
