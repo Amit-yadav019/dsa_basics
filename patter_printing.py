4ee59c0979c79037d09e8f5b7a19bc98dabb934e
Amit Kumar <2024ugpi019@nitjsr.ac.in>
2026-07-04 15:46:28 +0530

    Day 2 of learning pattern printing

--- patter_printing.py
+++ patter_printing.py
@@ -53,14 +53,120 @@
 #     print()    
 
 
-n = int(input("enter whats n "))
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(f" {i} ",end=' ')
-    print()   
+# n = int(input("enter whats n "))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(f" {i} ",end=' ')
+#     print()   
+
+
+#  1
+#  2   2
+#  3   3   3
+#  4   4   4   4
+#  5   5   5   5   5
+#  6   6   6   6   6   6
+#  7   7   7   7   7   7   7
+#  8   8   8   8   8   8   8   8  
 
 
 
+
+
+
+# n = int(input("whats the values of n : "))
+# for i in range(0,n):
+#     for j in range(0,n-i):
+#         print(" * ", end='')
+
+#     print()    
+
+#  *  *  *  *  * 
+#  *  *  *  * 
+#  *  *  * 
+#  *  * 
+#  * 
+
+
+
+# n = int(input("Enter n : "))
+# for i in range(0,n):
+#     for j in range(0,n-i):
+#         print(f" {j} ", end='')
+
+#     print()    
+
+# Output 
+#  0  1  2  3  4 
+#  0  1  2  3 
+#  0  1  2 
+#  0  1 
+#  0 
+
+
+
+# n = int(input("Enter n : "))
+# for i in range(1,n+2):
+#     for j in range(1,n+2-i):
+#         print(f" {j} ",end='')
+
+
+#     print()    
+
+# Enter n : 5
+#  1  2  3  4  5
+#  1  2  3  4
+#  1  2  3
+#  1  2
+#  1     
+
+
+
+
+# n = int(input(" whats the value of n : "))
+# for i in range(0,n):
+
+#     for j in range(0,n-i-1):
+#        print(" ",end='')
+#     for j in range(0,2*i+1):
+#         print("*",end='')
+#     for j in range(0,n-i-1):
+#         print(" ",end='')       
+#     print()
+
+#  whats the value of n : 8
+#        *
+#       ***
+#      *****
+#     *******
+#    *********
+#   ***********
+#  *************
+# ***************    
+       
+    
+
+
+n = int(input(" whats the value of n : "))
+for i in range(0,n):
+    for j in range(0,i):
+        print(" ",end='')
+    for j in range(0,2*n-2*i-1):
+        print("*",end='')
+    for j in range(0,i):
+        print(" ",end='')
+    print()            
+
+#  whats the value of n : 8
+# ***************
+#  ************* 
+#   ***********  
+#    *********   
+#     *******    
+#      *****     
+#       ***      
+#        *        
+      
 
 
 
