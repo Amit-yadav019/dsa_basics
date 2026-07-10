975c7a4536bf9217e57a7411c36a08f96836dcc5
Amit Kumar <2024ugpi019@nitjsr.ac.in>
2026-07-10 16:15:44 +0530

    Day 2 of learning basic maths and completed week 1 of ML

--- Basic_maths.py
+++ Basic_maths.py
@@ -36,18 +36,145 @@ def num_sum(n:int):
 #     n = int(input("Enter any number : "))
 #     print("number of digit = ",num_sum(n))  
 
-def rev_num(n:int):
+
+  # TO find the reverse of a number
+def num_rev(n:int):
     rev = 0
     while n>0:
-        lastdigit = n%10
-        rev = (rev * 10 )+ lastdigit
+        last_digit = n%10 # to find the last digit number 
+        rev = (rev*10)+last_digit
         n = n//10
     return rev
 
-if __name__ == "__main__":
-    n = int(input("Enter any number : "))
-    print("the reverse number is = ",rev_num(n)) 
+# if __name__ == "__main__" :
+#     n = int(input("whats the number n is : "))
+#     print("The reverse of this given number is :  ",num_rev(n))   
+
+
+# To find the number of Digit 
+
+def num_digit(n):
+    count = 0 
+    while n > 0:
+        last_digit = n%10
+        count = count + 1
+        n = n//10
+    return count
+# if __name__ =="__main__":
+#     n = int(input(" whats the number n is here :"))
+#     print(" The count of digits in the given number is =  ",num_digit(n))    
+
+# Alternate way by using a Logarithmic function 
+import math
+def num_digit_log(n):
+    
+    return int(math.log10(n)+1)
+# if __name__ =="__main__":
+#     n = int(input(" whats the number n is here :"))
+#     print(" The count of digits in the given number is =  ",num_digit_log(n))
+
+def palindrom(n:int):
+    rev = 0
+    original = n
+    while n >0:
+        last_digit = n%10
+        rev = (rev*10)+last_digit
+        n = n//10
+    if original == rev:
+        print(f" Yes, the given number {rev} is palindrome number  ")
+    else :
+        print(" Not a palindrome number ")
+    return rev
+# if __name__ == "__main__":
+#     n = int(input(" Enter any number to check for palindrome number "))
+#     palindrom(n)        
+
+import math
+def armstrong(n:int):
+    cube  = 0 
+    original = n 
+    while n>0 :
+        last_digit = n%10
+        cube = cube + (last_digit * last_digit * last_digit)
+        n = n//10
+        
+    if original == cube :
+        print(f" Yes the given number {n} is a Armstrong Number ")
+    else :
+        print(" oh no its not a armstrong number ")
+    return cube 
+# if __name__ == "__main__":
+#     n = int(input(" Enter any number to check for palindrome number "))
+#     armstrong(n)
+
+
+
+# To print all the divisors of a numbers 
+
+def divisors(n):
+    for i in range(1,n+1):
+        if n%i == 0 :
+            print(f" {i} ",end='')
+        
+# if __name__ == "__main__":
+#     n = int(input(" Enter Any number to see the divisors  "))
+#     divisors(n)                    
+
+            
+        # Alternate way with better T.C. time complexity 
+        # 
+import math
+def divisors2(n:int):
+    sqrt = math.isqrt(n)  
+  
+
+    for i in range(1,sqrt+1):
+        if n%i == 0 :
+            print(f" {i} ",end='')
+            if ((n//i)!= i):
+                
+                print(n//i,end='')
     
 
-    
+
+
+# if __name__ == "__main__":
+#     n = int(input(" Enter Any number to see the divisors  "))
+#     divisors2(n)            
+
+
+# if we wanted it in a sequence in ascending order then 
+
+import math
+def divisors2(n:int):
+    sqrt = math.isqrt(n)  
+    order = []
   
+
+    for i in range(1,sqrt+1):
+        if n%i == 0 :
+            print(f" {i} ",end='')
+            if ((n//i)!= i):
+                order.append(n//i)
+    for divisors in reversed(order):
+        print(f" {divisors} ",end='')            
+                
+                
+    
+
+
+
+if __name__ == "__main__":
+    n = int(input(" Enter Any number to see the divisors  "))
+    divisors2(n)     
+
+
+    
+
+
+
+
+
+
+
+
