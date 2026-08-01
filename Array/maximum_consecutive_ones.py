a07bb8d6f423ca36f3b9568ef16c8cb01d38464f
Amit Kumar <2024ugpi019@nitjsr.ac.in>
2026-08-01 23:28:59 +0530

    pushing to github after a week break

---
+++ Array/maximum_consecutive_ones.py
@@ -0,0 +1,13 @@
+def max_consecutive(arr):
+    n = len(arr)
+    count = 0 
+    max_count = 0
+    for num in arr:
+        if num == 1:
+            count = count + 1 
+        else :
+            count = 0 
+        max_count = max(max_count,count)    
+    return max_count
+arr = [1,1,0,1,1,1,0,1,1]
+print("The maximum consective ones are :" ,max_consecutive(arr))        
