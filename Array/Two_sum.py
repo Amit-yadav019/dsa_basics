faf0316c1562eec0932d02f03f497f431a4867b0
Amit Kumar <2024ugpi019@nitjsr.ac.in>
2026-08-03 13:31:02 +0530

    start learning for week 02

--- Array/Two_sum.py
+++ Array/Two_sum.py
@@ -1,10 +1,26 @@
-def two_sum(arr):
+
+  # Brute solution 
+# def two_sum(arr):
+#     n = len(arr)
+#     target = int(input("Enter your target value for two sum : "))
+#     for i in range(0,n):
+#         for j in range(i+1,n):
+#             if arr[i] + arr[j] == target :
+#                 return i,j
+#     return None
+# # arr = [5,7,9,1,2,4,15,7,3]
+# # print(two_sum(arr))  
+
+# optimal solution 
+def two_sum_optimal(arr):
     n = len(arr)
-    target = int(input("Enter your target value for two sum : "))
+    target = int(input("Enter the target value for two sum : "))
+    hash_map = {}
     for i in range(0,n):
-        for j in range(0,n):
-            if arr[i] + arr[j] == target :
-                return i,j
-    return i,j
+        remaining = target - arr[i]
+        if remaining in hash_map:
+            return [hash_map[remaining],i]
+        hash_map[arr[i]] = i
 arr = [5,7,9,1,2,4,15,7,3]
-print(two_sum(arr))        
+print(two_sum_optimal(arr))         
+
