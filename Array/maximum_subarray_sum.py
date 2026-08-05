fae42e12b0e9fe445365d2e82706d3486864861f
Amit Kumar <2024ugpi019@nitjsr.ac.in>
2026-08-05 23:02:20 +0530

    maximum subarray sum

--- Array/maximum_subarray_sum.py
+++ Array/maximum_subarray_sum.py
@@ -1,3 +1,4 @@
+# Brute soln.
 def max_subarray_sum(arr):
     n = len(arr)
  
@@ -9,6 +10,21 @@ def max_subarray_sum(arr):
             max_sum = max(max_sum,sum)
 
     return max_sum
-arr = [-2,1,-3,4,-1,2,1,-5,4]
-print("The max sum of subarray is : ",max_subarray_sum(arr))        
+# arr = [-2,1,-3,4,-1,2,1,-5,4]
+# print("The max sum of subarray is : ",max_subarray_sum(arr))        
 
+       # T.C. = O(n^2) and S.C. = O(1)
+
+
+def max_subarray_sum_optimal(arr):
+    n = len(arr)
+    max_sum = float("-inf")
+    sum = 0 
+    for i in range(0,n):
+        sum = sum + arr[i]
+        max_sum = max(max_sum,sum)
+        if sum<0:
+            sum = 0 
+    return max_sum
+arr = [-2,1,-3,4,-1,-2,1,-5,4]
+print("The max sum of subarray is : ",max_subarray_sum_optimal(arr)) 
