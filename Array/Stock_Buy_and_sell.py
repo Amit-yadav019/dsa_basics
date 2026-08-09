955be2b36d4c0b31cd3ff1adec3adfdc0454c2a3
Amit Kumar <2024ugpi019@nitjsr.ac.in>
2026-08-09 23:40:44 +0530

    nothing done in dsa

--- Array/Stock_Buy_and_sell.py
+++ Array/Stock_Buy_and_sell.py
@@ -28,6 +28,21 @@ def stock_buy_and_sell(prices):
               
     return max_profit
 prices = [7,2,1,5,6,4,8]
+# print("The max. profit is : ", stock_buy_and_sell(prices))
+
+
+def stock_buy_and_sell(prices):
+    n = len(prices)
+    max_profit = 0
+    for i in range(0,n):
+                                            # T.C. = O(n^2) and S.C. = O(1)
+        for j in range(i+1,n):
+            if prices[j] > prices[i]:
+                p = prices[j] - prices[i]
+                max_profit = max(max_profit,p)
+              
+    return max_profit
+prices = [7,2,1,5,6,4,8,14,57,78,89]
 print("The max. profit is : ", stock_buy_and_sell(prices))
 
             
