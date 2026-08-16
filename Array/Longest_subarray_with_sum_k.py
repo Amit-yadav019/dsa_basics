5c1fde560c131fc566608db91722874eddc68e04
Amit Kumar <2024ugpi019@nitjsr.ac.in>
2026-08-16 23:08:40 +0530

    less productive day

--- Array/Longest_subarray_with_sum_k.py
+++ Array/Longest_subarray_with_sum_k.py
@@ -1,17 +1,19 @@
-def longest_subarray(arr,k):
+def longest_subarray_with_sum_k(arr,k):
     n = len(arr)
-    l = 0 
+    length = 0
     for i in range(0,n):
-        
         for j in range(i,n):
             sum = 0
             for m in range(i,j+1):
                 sum = sum + arr[m]
-        if sum == m :
-            l = max(l,j-i+1)
-    return l 
+            if sum == k:
+                length = max(length,j-i+1)
+    return length 
+
 arr = [1,2,3,1,1,1,1,4,2,3]
-k = int(input("Enter the value of k : "))
-print("The longest sum of subarray with sum k is : ",longest_subarray(arr,k))                
+k = int(input("whats the value of k :  "))
+print(f"The longest subarray with sum {k} is : ",longest_subarray_with_sum_k(arr,k))                
+
+               
 
             
