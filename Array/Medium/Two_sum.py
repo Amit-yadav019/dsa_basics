5aacd8d67bf9cd1fa4496ba4087868795b9399b5
Amit Kumar <2024ugpi019@nitjsr.ac.in>
2026-08-18 23:25:52 +0530

    Two sum problem and completed week 01 of unsupervised learning

--- Array/Medium/Two_sum.py
+++ Array/Medium/Two_sum.py
@@ -2,7 +2,7 @@ def two_sum(arr):
     n = len(arr)
     Target = int(input("Whats your target value : "))
     for i in range(0,n):
-        sum = 0 
+        
         for j in range(i+1,n):
             if Target == arr[i] + arr[j]:
                 return i,j
