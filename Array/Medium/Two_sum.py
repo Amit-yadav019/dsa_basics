86daf2ef75e3134b0d976c00ae7ae4adab6c822e
Amit Kumar <2024ugpi019@nitjsr.ac.in>
2026-08-19 23:24:56 +0530

    Two sum problem

--- Array/Medium/Two_sum.py
+++ Array/Medium/Two_sum.py
@@ -8,4 +8,21 @@ def two_sum(arr):
                 return i,j
     return False
 arr = [2,6,5,8,11]
-print(two_sum(arr))        
+# print(two_sum(arr))     # T.C. = O(N^2) and S.C. = O(1)
+
+# Better soln. using Hashmap Dictonary
+
+def two_sum_better(arr):
+    n = len(arr)
+    hash_map = {}
+    
+    for i in range(0,n):
+        remaining = Target - arr[i]
+        if remaining in hash_map:
+            return [hash_map[remaining],i]
+        else:
+            hash_map[arr[i]]= i
+    return False
+arr = [2,6,5,8,11]
+Target = int(input("Enter the target value that you wanted : "))
+print(two_sum_better(arr))    
