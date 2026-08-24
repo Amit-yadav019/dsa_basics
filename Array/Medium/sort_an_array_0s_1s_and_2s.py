981274d42afb53ad248aeebbafbdf0569316a6cb
Amit Kumar <2024ugpi019@nitjsr.ac.in>
2026-08-24 23:22:15 +0530

    about to complete ml theory by andrew ng

--- Array/Medium/sort_an_array_0s_1s_and_2s.py
+++ Array/Medium/sort_an_array_0s_1s_and_2s.py
@@ -1,4 +1,4 @@
-def sort(arr):
+def sort(arr): # This is the better solution .
     n = len(arr)
     count0 = 0
     count1 = 0
@@ -17,5 +17,33 @@ def sort(arr):
     for i in range(count1+count0,count0+count1+count2):
         arr[i] = 2
     return arr
+# arr = [0,1,2,0,1,2,1,2,0,0,1,0,0,1,2]
+# print(sort(arr)) 
+# Brute solution will be by using merge sort and it will take a T.C. = O(NlogN) and S.C. = O(N)
+
+
+# Now solving Optimal solution by using 
+# # Dutch National Flag Algorithm 
+def Sort_optimal(arr):
+    n = len(arr)
+    low = 0 
+    mid = 0 
+    high = n - 1
+    while(mid<=high):
+        if arr[mid]==0:
+            arr[low],arr[mid]=arr[mid],arr[low]
+            low = low +1
+            mid = mid +1
+        elif arr[mid] == 1:
+            mid = mid + 1
+        else:
+            arr[mid],arr[high] = arr[high],arr[mid]
+            high = high -1 
+    return arr
 arr = [0,1,2,0,1,2,1,2,0,0,1,0,0,1,2]
-print(sort(arr))
+print(Sort_optimal(arr))
+           
+
+         
+            
+        
